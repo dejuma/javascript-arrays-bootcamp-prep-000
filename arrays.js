d40ed36a099aff821212e4c091e59756721df9b1
Oluwadunsin Joseph Olesin <josepholesin@hotmail.com>
2018-07-20 23:49:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,16 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
+
 function addElementToBeginningOfArray(array, element) {
-  var array2 = [element, ...array]
-  return array
+  var nameArray = [1]
+  nameArray.unshift("foo");
+  return nameArray
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  var nameArray = [1]
+  nameArray.unshift("foo");
+  return nameArray
+
 }
-function addElementToEndOfArray(array, element) {
-  [...array, element]
-  return [...array, element]
-}
+
